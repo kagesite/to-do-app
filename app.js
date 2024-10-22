adc08d689cb1913a32f526dd76046a1e5142b509
Kage Saluone <ksaluone17@gmail.com>
2024-10-21 21:07:27 -0600

    Created addMainList function that adds a newList to the mainLists array

--- app.js
+++ app.js
@@ -7,4 +7,21 @@ function saveToLocalStorage() {
     localStorage.setItem('mainLists', JSON.stringify(mainLists));
 }
 
+// Create a function to add list to mainLists;
+function addMainList() {
+    const mainListName = document.getElementById('mainListInput').value;
+
+    if (mainListName) {
+        const newList = {
+            name: mainListName,
+            subTasks: []
+        }
+
+        mainLists.push(newList)
+        document.getElementById('mainListInput').value = '';
+        saveToLocalStorage();
+        // displayMainLists();
+    }
+}
+
 // localStorage.clear();

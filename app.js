87f2fdc2d57e5616fdaae4dc1ecbd403bfb079c0
Kage Saluone <ksaluone17@gmail.com>
2024-10-21 21:04:42 -0600

    Created base variables and a saveToLocalStorage function

---
+++ app.js
@@ -0,0 +1,10 @@
+// Create variable to hold all the Main Lists in 
+let mainLists = JSON.parse(localStorage.getItem('mainLists')) || [];
+let selectedListIndex = null;
+
+// Create a function to save data to localStorage
+function saveToLocalStorage() {
+    localStorage.setItem('mainLists', JSON.stringify(mainLists));
+}
+
+// localStorage.clear();
